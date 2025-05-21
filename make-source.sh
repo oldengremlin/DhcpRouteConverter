cd3647a969b02324aaee2b692b212ad46e7aefaa
Alexander Russkih <oldengremlin@gmail.com>
2025-05-21 07:59:48 +0300

    Version 3.0.0

--- make-source.sh
+++ make-source.sh
@@ -22,7 +22,7 @@ echo "" > /dev/stderr
 SOURCENAME=source.txt
 cp /dev/null ${SOURCENAME}
 echo "Вміст файлу ${SOURCENAME}:" > /dev/stderr
-for F in $( find src/deb/control/ -type f  -print ) $( find src/main/resources/ -type f -print ) ./pom.xml ./nb-configuration.xml ./README.md ./routers.yaml; do
+for F in $( find src/deb/control/ -type f  -print ) $( find src/main/resources/ -type f -print ) $( find .github/ -type f  -print ) $( find src/main/resources/ -type f -print ) ./pom.xml ./nb-configuration.xml ./README.md ./routers.yaml; do
     echo -n "• ${F} строки з "`wc -l ${SOURCENAME} | awk '{ printf("%s",$1) }'`" по " > /dev/stderr
     echo "// File: " ${F} >>${SOURCENAME}
     cat ${F} | sed "/^ *$/d" >>${SOURCENAME}
